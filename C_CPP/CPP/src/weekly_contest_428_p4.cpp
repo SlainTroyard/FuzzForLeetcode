35725d864eee54210d2dafd93daaea38488f7e2e
xfliu <xfliu@whu.edu.cn>
2025-01-02 02:21:17 +0800

    428 4

---
+++ C_CPP/CPP/src/weekly_contest_428_p4.cpp
@@ -0,0 +1,80 @@
+// Problem: Weekly Contest 428 Problem 4
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+const int INF = 1e6;
+
+class Solution {
+public:
+    int makeStringGood(string s) {
+        int n = s.size();
+        vector<int> a(26);
+        for (char c : s) {
+            a[c - 'a']++;
+        }
+
+        int ans = n;
+        for (int x : a) {
+            ans = min(ans, n - x);
+        }
+
+        for (int k = 1; k <= n; k++) {
+            vector<int> dp(26, -1);
+            ans = min(ans, dfs(dp, a, k, 0));
+        }
+
+        return ans;
+    }
+
+    int dfs(vector<int>& dp, vector<int>& a, int k, int i) {
+        if (i >= a.size()) {
+            return 0;
+        }
+
+        if (dp[i] != -1) {
+            return dp[i];
+        }
+
+        int ans = INF;
+        if (a[i] >= k) {
+            int more = a[i] - k;
+            ans = min(ans, a[i] - k + dfs(dp, a, k, i + 1));
+            if (i + 1 < a.size() && a[i + 1] <= k) {
+                if (a[i + 1] + more >= k) {
+                    ans = min(ans, more + dfs(dp, a, k, i + 2));
+                } else {
+                    ans = min(ans, min(more + k - (a[i + 1] + more), more + a[i + 1]) + dfs(dp, a, k, i + 2));
+                }
+            }
+
+        } else {
+            ans = min(ans, min(k - a[i], a[i]) + dfs(dp, a, k, i + 1));
+            if (i + 1 < a.size() && a[i + 1] <= k) {
+                if (a[i + 1] + a[i] >= k) {
+                    ans = min(ans, a[i] + dfs(dp, a, k, i + 2));
+                } else {
+                    ans = min(ans, a[i] + (k - (a[i] + a[i + 1])) + dfs(dp, a, k, i + 2));
+                }
+            }
+        }
+
+        return dp[i] = ans;
+    }
+};
+
+int main() {
+    Solution solution;
+    string s;
+
+    // Input the string
+    cin >> s;
+
+    // Call makeStringGood and output the result
+    int result = solution.makeStringGood(s);
+    cout << result << endl;
+
+    return 0;
+}
