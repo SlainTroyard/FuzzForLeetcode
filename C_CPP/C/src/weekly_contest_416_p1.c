d4d8cf965474e30457326f0def3e8d368258697d
Charmian <2751237029@qq.com>
2025-01-09 13:34:54 +0800

    add 416 1

---
+++ C_CPP/C/src/weekly_contest_416_p1.c
@@ -0,0 +1,48 @@
+// Problem: Weekly Contest 416 Problem 1
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+int cmp(const void *a, const void *b){
+    return strcmp(*(char**)a, *(char**)b);
+}
+
+bool reportSpam(char** message, int messageSize, char** bannedWords, int bannedWordsSize){
+    qsort(bannedWords, bannedWordsSize, sizeof(char*), cmp);
+    int cnt = 0;
+    for (int i = 0; i < messageSize; i++)
+    {
+        if (bsearch(&message[i], bannedWords, bannedWordsSize, sizeof(char*), cmp) != NULL)
+        {
+            cnt++;
+        }
+        if (cnt >= 2)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main() {
+    int messageSize, bannedWordsSize;
+    scanf("%d", &messageSize);
+    char **message = (char**)malloc(sizeof(char*) * messageSize);
+    for (int i = 0; i < messageSize; i++)
+    {
+        message[i] = (char*)malloc(sizeof(char) * 100);
+        scanf("%s", message[i]);
+    }
+    scanf("%d", &bannedWordsSize);
+    char **bannedWords = (char**)malloc(sizeof(char*) * bannedWordsSize);
+    for (int i = 0; i < bannedWordsSize; i++)
+    {
+        bannedWords[i] = (char*)malloc(sizeof(char) * 100);
+        scanf("%s", bannedWords[i]);
+    }
+    printf("%d\n", reportSpam(message, messageSize, bannedWords, bannedWordsSize));
+    free(message);
+    free(bannedWords);
+    return 0;
+}
---
+++ C_CPP/CPP/src/weekly_contest_416_p1.cpp
@@ -0,0 +1,37 @@
+// Problem: Weekly Contest 416 Problem 1
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+    bool reportSpam(vector<string>& message, vector<string>& bannedWords) {
+        unordered_set<string> banned(bannedWords.begin(), bannedWords.end());
+        int cnt = 0;
+        for (auto& s : message) {
+            if (banned.find(s) != banned.end() && ++cnt > 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+int main() {
+    int messageSize, bannedWordsSize;
+    cin >> messageSize;
+    vector<string> message(messageSize);
+    for (int i = 0; i < messageSize; i++) {
+        cin >> message[i];
+    }
+    cin >> bannedWordsSize;
+    vector<string> bannedWords(bannedWordsSize);
+    for (int i = 0; i < bannedWordsSize; i++) {
+        cin >> bannedWords[i];
+    }
+    Solution s;
+    cout << s.reportSpam(message, bannedWords) << endl;
+    return 0;
+}
