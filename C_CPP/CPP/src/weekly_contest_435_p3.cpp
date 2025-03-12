b6c66a253c71b7f786566e68e3bc2f96e39cc323
Charmian <2751237029@qq.com>
2025-03-12 17:08:30 +0800

    add 435 3 CPP

---
+++ C_CPP/CPP/src/weekly_contest_435_p3.cpp
@@ -0,0 +1,55 @@
+// Problem: Weekly Contest 435 Problem 3
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+#include <climits>
+#include <functional>
+using namespace std;
+
+class Solution {
+public:
+    int minimumIncrements(vector<int>& nums, vector<int>& target) {
+        int n = nums.size(), m = target.size();
+
+        long long g[1 << m];
+        for (int i = 0; i < (1 << m); i++) {
+            g[i] = 1;
+            for (int j = 0; j < m; j++) if (i >> j & 1)
+                g[i] = g[i] / gcd(g[i], target[j]) * target[j];
+        }
+
+        const long long INF = 1e18;
+        long long f[2][1 << m];
+        for (int j = 0; j < (1 << m); j++) f[0][j] = INF;
+        f[0][0] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < (1 << m); j++) f[i & 1][j] = f[i & 1 ^ 1][j];
+            for (int j = 0; j < (1 << m); j++) for (int k = j; k > 0; k = (k - 1) & j) {
+                long long v = (nums[i - 1] + g[k] - 1) / g[k] * g[k] - nums[i - 1];
+                f[i & 1][j] = min(f[i & 1][j], f[i & 1 ^ 1][j ^ k] + v);
+            }
+        }
+
+        return f[n & 1][(1 << m) - 1];
+    }
+};
+
+int main() {
+    // TODO: Add the base I/O interface here
+    int n, m;
+    cin >> n >> m;
+    vector<int> nums(n), target(m);
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+    for (int i = 0; i < m; i++) {
+        cin >> target[i];
+    }
+    Solution solution;
+    cout << solution.minimumIncrements(nums, target) << endl;
+    return 0;
+}
--- todo.md
+++ todo.md
@@ -16,6 +16,7 @@
 - 434 P3 lacks C edition, src: Can't find a solution in the C edition.
 - 434 P4 lacks C edition, src: Can't find a solution in the C edition.
 - 435 P2 lacks C edition, src: Can't find a solution in the C edition.
+- 435 P3 lacks C edition, src: Can't find a solution in the C edition.
 
 ## add contests
 
