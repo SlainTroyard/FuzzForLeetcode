bef69e94de0a122347778ccb5bcd0b420ef4c0f8
Charmian <2751237029@qq.com>
2025-03-12 15:04:28 +0800

    add 434 1

---
+++ C_CPP/C/src/weekly_contest_434_p1.c
@@ -0,0 +1,35 @@
+// Problem: Weekly Contest 434 Problem 1
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+int countPartitions(int* nums, int numsSize) {
+    int count = 0;
+    for (int i = 0; i < numsSize - 1; i++) {
+        int leftSum = 0, rightSum = 0;
+        for (int j = 0; j <= i; j++) {
+            leftSum += nums[j];
+        }
+        for (int j = i + 1; j < numsSize; j++) {
+            rightSum += nums[j];
+        }
+        if ((leftSum - rightSum) % 2 == 0) {
+            count++;
+        }
+    }
+    return count;
+}
+
+int main() {
+    // TODO: Add the base I/O interface here
+    int n;
+    scanf("%d", &n);
+    int* nums = (int*)malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &nums[i]);
+    }
+    printf("%d\n", countPartitions(nums, n));
+    free(nums);
+    return 0;
+}
---
+++ C_CPP/CPP/src/weekly_contest_434_p1.cpp
@@ -0,0 +1,28 @@
+// Problem: Weekly Contest 434 Problem 1
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+using namespace std;
+
+class Solution {
+public:
+    int countPartitions(vector<int>& nums) {
+        int s = reduce(nums.begin(), nums.end());
+        return s % 2 ? 0 : nums.size() - 1;
+    }
+};
+
+int main() {
+    // TODO: Add the base I/O interface here
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+    Solution sol;
+    cout << sol.countPartitions(nums) << endl;
+    return 0;
+}
