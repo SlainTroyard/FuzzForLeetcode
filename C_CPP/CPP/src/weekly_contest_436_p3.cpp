860a5dcd14b433ad34918a694f9a5ee56e0e33b6
Charmian <2751237029@qq.com>
2025-03-12 21:09:23 +0800

    add 366 3

---
+++ C_CPP/CPP/src/weekly_contest_436_p3.cpp
@@ -0,0 +1,37 @@
+// Problem: Weekly Contest 436 Problem 3
+#include <iostream>
+#include <string>
+#include <vector>
+#include <array>
+
+using namespace std;
+
+class Solution {
+public:
+    long long countSubstrings(string s) {
+        long long ans = 0;
+        array<array<int, 9>, 10> f{};
+        for (char c : s) {
+            int d = c - '0';
+            for (int m = 1; m < 10; ++m) {
+                array<int, 9> nf{};
+                nf[d % m] = 1;
+                for (int rem = 0; rem < m; ++rem) {
+                    nf[(rem * 10 + d) % m] += f[m][rem];
+                }
+                f[m] = nf;
+            }
+            ans += f[d][0];
+        }
+        return ans;
+    }
+};
+
+int main() {
+    // TODO: Add the base I/O interface here
+    string s;
+    cin >> s;
+    Solution sol;
+    cout << sol.countSubstrings(s) << endl;
+    return 0;
+}
--- todo.md
+++ todo.md
@@ -18,6 +18,7 @@
 - 435 P2 lacks C edition, src: Can't find a solution in the C edition.
 - 435 P3 lacks C edition, src: Can't find a solution in the C edition.
 - 435 P4 lacks C edition, src: Can't find a solution in the C edition.
+- 436 P3 lacks C edition, src: Can't find a solution in the C edition.
 
 ## add contests
 
