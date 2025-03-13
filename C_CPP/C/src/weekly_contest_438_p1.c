e888a712faf6d403ab11bc5f63efdf440167d37c
Charmian <2751237029@qq.com>
2025-03-13 10:48:26 +0800

    add 438 1

---
+++ C_CPP/C/src/weekly_contest_438_p1.c
@@ -0,0 +1,22 @@
+// Problem: Weekly Contest 438 Problem 1
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+bool hasSameDigits(char* s) {
+    int len = strlen(s), i = 0;
+    while (i < len) s[i ++] &= 0x0f;
+    while (-- len > 1)
+        for (i = 0; i < len; ++ i)
+            s[i] = (s[i] + s[i + 1]) % 10;
+    return s[0] == s[1];
+}
+
+int main() {
+    // TODO: Add the base I/O interface here
+    char s[100];
+    scanf("%s", s);
+    printf("%d\n", hasSameDigits(s));
+    return 0;
+}
---
+++ C_CPP/CPP/src/weekly_contest_438_p1.cpp
@@ -0,0 +1,30 @@
+// Problem: Weekly Contest 438 Problem 1
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool hasSameDigits(string s) {
+        while (s.size() > 2) {
+            string t;
+            for (int i = 0; i + 1 < s.size(); i++) {
+                int x = s[i] - '0', y = s[i + 1] - '0';
+                t.push_back((x + y) % 10 + '0');
+            }
+            s = t;
+        }
+        return s[0] == s[1];
+    }
+};
+    
+
+int main() {
+    // TODO: Add the base I/O interface here
+    string s;
+    cin >> s;
+    Solution sol;
+    cout << sol.hasSameDigits(s) << endl;
+    return 0;
+}
